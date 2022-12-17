1b978ec76df4b9d359e0086453dce25b49847052
Helder Duarte <heldertz2@gmail.com>
2022-12-17 18:54:53 +0000

    Day 4 second part complete

--- src/bin/04/04.rs
+++ src/bin/04/04.rs
@@ -7,10 +7,6 @@ struct Interval {
 }
 
 impl Interval {
-    fn new(start: usize, end: usize) -> Self {
-        Self {start, end}
-    }
-
     fn from_str(s: &str) -> Self {
         let mut it = s.split('-');
         Self {
@@ -21,25 +17,27 @@ impl Interval {
 }
 
 fn contains(int1: &Interval, int2: &Interval) -> bool {
-    if     (int1.start <= int2.start && int1.end >= int2.end)
-        || (int2.start <= int1.start && int2.end >= int1.end)
-    {
-        // println!("Found overlap {:?} {:?}", int1, int2);
-        return true;
-    }
-    // println!("Did not find overlap {:?} {:?}", int1, int2);
-    return false;
+    (int1.start <= int2.start && int1.end >= int2.end) ||
+    (int2.start <= int1.start && int2.end >= int1.end)
+}
+
+fn overlaps(int1: &Interval, int2: &Interval) -> bool {
+    (int1.start <= int2.start && int2.start <= int1.end) ||
+    (int2.start <= int1.start && int1.start <= int2.end)
 }
 
 fn main() {
+    let mut count_full_overlaps = 0;
     let mut count_overlaps = 0;
     for (_i, line) in ASSIGNMENTS.lines().enumerate() {
         let mut it = line.split(',');
         let int1 = Interval::from_str(it.next().unwrap());
         let int2 = Interval::from_str(it.next().unwrap());
         // println!("{} {:?} {:?}", _i+1, int1, int2);
-        count_overlaps += if contains(&int1, &int2) { 1 } else { 0 }; 
+        count_full_overlaps += if contains(&int1, &int2) { 1 } else { 0 };
+        count_overlaps += if overlaps(&int1, &int2) { 1 } else { 0 };
     }
 
+    println!("{}", count_full_overlaps);
     println!("{}", count_overlaps);
 }
